54d3723203f09a4b01a577a24b41d527a1ff2fbc
Eric Penn <ep00959@georgiasouthern.edu>
2019-12-18 16:42:35 -0500

    updated menu models

--- models/menu.js
+++ models/menu.js
@@ -8,9 +8,33 @@ const MenuSchema = new mongoose.Schema({
   alcoholAvailable: Boolean
 })
 
+const MenuCollection = mongoose.model('menu', MenuSchema)
 
+const getMenuById = menuId => {
+  return MenuCollection.findOne({ _id: menuId })
+}
 
+const getAllMenus = () => {
+  return MenuCollection.find({})
+}
+
+const createMenu = (newMenu) => {
+  return MenuCollection.create(newMenu)
+}
+
+const updateMenu = (menuId, updatedMenu) => {
+  return MenuCollection.updateOne({ _id: menuId }, updatedMenu)
+}
+
+const deleteMenu = (menuId) => {
+  return MenuCollection.deleteOne({ _id: menuId })
+}
 
 module.exports = {
+  getMenuById,
+  getAllMenus,
+  createMenu,
+  updateMenu,
+  deleteMenu
 
 }
